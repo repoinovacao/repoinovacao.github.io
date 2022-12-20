06f73b6b788856f199854b58a560120caaf1dd5f
José Nogueira <42451402+josenogueiradalmeida@users.noreply.github.com>
2022-12-20 14:40:36 -0300

    troca a letra

--- publicaNoSiteBNDES.bat
+++ publicaNoSiteBNDES.bat
@@ -1,17 +1,17 @@
 @ECHO OFF
 CLS
 SET DIR_DESTINO="\\bndes.net\bndes\Aplicacoes\SiteBNDES\4.PRD\arquivos\metaverso"
-SUBST F: %DIR_DESTINO%
+SUBST G: %DIR_DESTINO%
 ECHO.
 ECHO Esse script ira sobreescrever os arquivos no Site do BNDES. Caso nao deseje continuar, pressione CTRL+C. Caso queira continuar, pressione qualquer outra tecla.
 ECHO.
 PAUSE
 ECHO.
-XCOPY /Y * F:
-XCOPY /E /Y assets F:\assets
-XCOPY /E /Y libs F:\libs
-XCOPY /E /Y tour-360 F:\tour-360
-XCOPY /E /Y video-360 F:\video-360
+XCOPY /Y * G:
+XCOPY /E /Y assets G:\assets
+XCOPY /E /Y libs G:\libs
+XCOPY /E /Y tour-360 G:\tour-360
+XCOPY /E /Y video-360 G:\video-360
 ECHO.
 ECHO Tudo copiado!
 ECHO.
