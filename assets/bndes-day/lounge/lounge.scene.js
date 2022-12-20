053837d98d32db0069abf41148843ab04defa747
José Nogueira <42451402+josenogueiradalmeida@users.noreply.github.com>
2022-12-20 19:54:06 -0300

    Lounge: add avatar com balao

--- assets/bndes-day/lounge/lounge.scene.js
+++ assets/bndes-day/lounge/lounge.scene.js
@@ -37,13 +37,13 @@ export default {
         //     width: 1.2 * 2,
         //     height: 3 * 2,
         // },
-        {
-            src: '#speech',
-            scene: "#lounge",
-            position: fromCylindrical(6, -80, 2.9),
-            rotation: "0 0 0",
-            animation: floatAnimationCylindrical(6, -80, 2.9),
-        },
+        // {
+        //     src: '#speech',
+        //     scene: "#lounge",
+        //     position: fromCylindrical(6, -80, 2.9),
+        //     rotation: "0 0 0",
+        //     animation: floatAnimationCylindrical(6, -80, 2.9),
+        // },
         {
             scene: "#lounge--salas-direita",
             position: fromCylindrical(10, 0, WAYPOINT_HEIGHT),
@@ -108,18 +108,16 @@ export default {
     //     }
     // ],
     images: [
-        //  {
-        //      // src: "../assets/bndes-day/mapa.jpg",
-        //      src: "#mapa",
-        //      position: fromCylindrical(8.0, -60, 0.2),
-        //     //  position: fromCylindrical(3.0, -135, 0.2),
-        //      rotation: "0 0 0.5",
-        //      width: 4.104,
-        //      height: 1.854
-        //     // width: 6.840 * .35,
-        //     // height: 3.090 *.35,
-        // },
+         {
+             src: "../assets/bndes-day/lounge/speech.png",
+             position: fromCylindrical(6, -80, 3.9),
+             rotation: "0 0 0",
+             animation: floatAnimationCylindrical(6, -80, 3.9),
+             width: 1280 * 0.003,
+             height: 720 * 0.003
+        },
      ],
+     
     // videos: [
     //     {
     //         src: "../assets/dica_mpme_1.mp4",
@@ -129,23 +127,32 @@ export default {
     //         width: 1,
     //     }
     // ],
+    // models: [
+    //     // {
+    //     //     // src: "#robot",
+    //     //     src: "../assets/robot.gltf",
+    //     //     href: "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_MmE0NTZjYWItYjc2NS00OWM4LWFiMjYtYzkxYjRkMzllMTU5%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%25227e2324c6-6925-427e-b56d-4e6eda16752a%2522%252c%2522Oid%2522%253a%2522e1573abf-a8c0-4b96-91d5-346a95008d9a%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=9a82c16a-b7a5-467d-9f0b-43756abe0d2e&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true",
+    //     //     scale: "3 3 3",
+    //     //     position: fromCylindrical(13, -120, -2.2),
+    //     //     rotation: "0 30 0",
+    //     // },
+    //     // {
+    //     //     // src: "#mapa3d",
+    //     //     src: "../assets/bndes-day/mapa3d.gltf",
+    //     //     href: "https://chatbot.bndes.gov.br/atendimento",
+    //     //     scale: "1.5 1.5 1.5",
+    //     //     position: fromCylindrical(9, -110, -2.2),
+    //     //     rotation: "0 10 0",
+    //     // }
+    // ],
     models: [
-        // {
-        //     // src: "#robot",
-        //     src: "../assets/robot.gltf",
-        //     href: "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_MmE0NTZjYWItYjc2NS00OWM4LWFiMjYtYzkxYjRkMzllMTU5%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%25227e2324c6-6925-427e-b56d-4e6eda16752a%2522%252c%2522Oid%2522%253a%2522e1573abf-a8c0-4b96-91d5-346a95008d9a%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=9a82c16a-b7a5-467d-9f0b-43756abe0d2e&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true",
-        //     scale: "3 3 3",
-        //     position: fromCylindrical(13, -120, -2.2),
-        //     rotation: "0 30 0",
-        // },
-        // {
-        //     // src: "#mapa3d",
-        //     src: "../assets/bndes-day/mapa3d.gltf",
-        //     href: "https://chatbot.bndes.gov.br/atendimento",
-        //     scale: "1.5 1.5 1.5",
-        //     position: fromCylindrical(9, -110, -2.2),
-        //     rotation: "0 10 0",
-        // }
+        {            
+            //https://sketchfab.com/3d-models/business-man-low-polygon-game-character-b6f6740f883b4749abac47af0045a9dd
+            src: "#presi",
+            scale: "2.1 2.1 2.1",
+            position: "0 -1.5 -4",
+            // rotation: "0 -53.19 0",
+        }
     ]
 
 }
